e130d7b813d3ac0a5ff7bd74be7a023baf42b57c
Josh <24633211+JFC1@users.noreply.github.com>
2023-01-23 15:44:56 -0800

    added json static files and views to API that will need to be changed to work with JSON instead of CSV

--- capstone/api/views.py
+++ capstone/api/views.py
@@ -1,3 +1,8 @@
+import json
+import requests
+from csv import DictReader
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
 from rest_framework import generics
 from .models import State
 from .serializers import ApiSerializer
@@ -15,3 +20,24 @@ class CreateState(generics.CreateAPIView):
 class StateView(generics.RetrieveUpdateDestroyAPIView):
     queryset = State.objects.all()
     serializer_class = ApiSerializer
+
+
+# Opens Tax Foundation website to webscrape effective tax burdens
+with urlopen('https://taxfoundation.org/tax-burden-by-state-2022/#results') as response:
+    soup_tax = BeautifulSoup(response, 'html.parser')   # parses HTML data webpage listed above
+
+# Opens CSV files to access state data
+politics_csv = r'C:\Users\joshg\pdx_code\class_opal\code\josh\python\josh_mini_capstone\politics.csv'
+with open(politics_csv, 'r') as file:
+    politics_dict = DictReader(file) # stores CSV data as dictionaries
+    politics_list = list(politics_dict)  # stores dictionaries from CSV data in a list
+
+homes_csv = r'C:\Users\joshg\pdx_code\class_opal\code\josh\python\josh_mini_capstone\homes.csv'
+with open(homes_csv, 'r') as file:
+    homes_dict = DictReader(file)
+    homes_list = list(homes_dict)
+
+crime_csv = r'C:\Users\joshg\pdx_code\class_opal\code\josh\python\josh_mini_capstone\crime.csv'
+with open(crime_csv, 'r') as file:
+    crime_dict = DictReader(file)
+    crime_list = list(crime_dict)
