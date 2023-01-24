cb0132cae83c354e355b5d0c2c33171ce63ef6cd
Josh <24633211+JFC1@users.noreply.github.com>
2023-01-23 16:28:37 -0800

    converted functions from reading CSVs to reading JSONs

--- capstone/api/models.py
+++ capstone/api/models.py
@@ -1,4 +1,4 @@
 from django.db import models
 
 class State(models.Model):
-    state_name = models.CharField(max_length=2)
+    state_name = models.CharField(max_length=50)
--- capstone/api/views.py
+++ capstone/api/views.py
@@ -1,5 +1,5 @@
 import json
-import requests
+# import requests
 from csv import DictReader
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
@@ -24,20 +24,17 @@ class StateView(generics.RetrieveUpdateDestroyAPIView):
 
 # Opens Tax Foundation website to webscrape effective tax burdens
 with urlopen('https://taxfoundation.org/tax-burden-by-state-2022/#results') as response:
-    soup_tax = BeautifulSoup(response, 'html.parser')   # parses HTML data webpage listed above
+    soup_tax = BeautifulSoup(response, 'html.parser')   # parses HTML data from webpage listed above
 
-# Opens CSV files to access state data
-politics_csv = r'C:\Users\joshg\pdx_code\class_opal\code\josh\python\josh_mini_capstone\politics.csv'
-with open(politics_csv, 'r') as file:
-    politics_dict = DictReader(file) # stores CSV data as dictionaries
-    politics_list = list(politics_dict)  # stores dictionaries from CSV data in a list
+# Opens JSON files to access state data
+politics_json = 'capstone\api\static\api\politics.json'
+with open(politics_json, 'r') as file:
+    politics_list = json.load(file)
 
-homes_csv = r'C:\Users\joshg\pdx_code\class_opal\code\josh\python\josh_mini_capstone\homes.csv'
-with open(homes_csv, 'r') as file:
-    homes_dict = DictReader(file)
-    homes_list = list(homes_dict)
+homes_json = 'capstone\api\static\api\homes.json'
+with open(homes_json, 'r') as file:
+    homes_list = json.load(file)
 
-crime_csv = r'C:\Users\joshg\pdx_code\class_opal\code\josh\python\josh_mini_capstone\crime.csv'
-with open(crime_csv, 'r') as file:
-    crime_dict = DictReader(file)
-    crime_list = list(crime_dict)
+crime_json = 'capstone\api\static\api\crime.json'
+with open(crime_json, 'r') as file:
+    crime_list = json.load(file)
