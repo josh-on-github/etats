1e9f51fba52e6ade6c85091f11edd4c81f57477e
Josh <24633211+JFC1@users.noreply.github.com>
2023-01-26 11:29:32 -0800

    working on views and form tag for etats app

--- capstone/etats/views.py
+++ capstone/etats/views.py
@@ -6,21 +6,26 @@ from .models import Rank
 from api.models import State
 
 
-# def index(request):
-#     if request.method == 'POST':
-#         form_data = request.POST
-#         rank_object = Rank.objects.create(
-#             state_name=form_data['state_name'], 
-#             weighted_rank=form_data['weighted_rank'],
-#         )
-#         return HttpResponseRedirect(reverse('etats:index'))
-
-#     else:
-#         rank_list = Rank.objects.all()
-#         context = {'rank_list': rank_list}
-#         return render(request, 'etats/index.html', context)
-
-
 def index(request):
-    print(Rank.objects.all())
-    return HttpResponse("Hello, world")
+    if request.method == 'POST':
+        form_data = request.POST
+        state_object = None
+        for item in form_data:
+            if form_data[item] != '':
+                state_object = State.objects.get(item=form_data[item])  ### need to fix item to state model field ###
+        rank_object = Rank.objects.create(
+            state_name=form_data['state_name'], 
+            weighted_rank=form_data['weighted_rank'],
+        )
+        context = {'state_object': state_object}
+        return render(request, 'etats/index.html', context)
+
+    else:
+        rank_list = Rank.objects.all()
+        context = {'rank_list': rank_list}
+        return render(request, 'etats/index.html', context)
+
+
+# def index(request):
+#     print(Rank.objects.all())
+#     return HttpResponse("Hello, world")
