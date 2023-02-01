647d365793adb81e88008e7a5fce9678669f74de
Josh <24633211+JFC1@users.noreply.github.com>
2023-02-01 12:09:39 -0800

    updated html for mounted function on each file

--- capstone/etats/static/etats/vue.js
+++ capstone/etats/static/etats/vue.js
@@ -12,24 +12,14 @@ new Vue({
     mounted() {
         this.getStateObject()
     },
-    // mounted: () => {
-        // this.getStateObject()
-        // // console.log('Test')
-            // console.log('GET Request');
-            // axios.get('/api')
-            // .then(response => console.log(response.data))
-            // .then(response => this.listItems = response.data)
-                // this.listItems.push(response.data))
-    // },
     methods: {
-        // searches Free Dictionary API with user input and loads definition 
+        // searches API with user input and loads state info 
         getStateObject() {
             console.log('GET Request');
             axios.get('/api')
             .then(response => this.listItems = response.data)
-            // .meanings[0].definitions[0].definition)
             .then(data => console.log(data))
-            // alerts user if user input is not found in dictionary
+            // alerts user if user input is not found in API
             .catch(error => this.searchError = true && alert('Your search did not produce any results.  Please check your spelling and try again.'))
         },
         // Appends user input to 'listItems' array and clears input field
@@ -76,5 +66,24 @@ new Vue({
         // Item is deleted from list
         deleteItem (todoItem) {
             this.listItems.splice(todoItem, 1)
-        }
+        },
+        // clears unsaved user input from search field, loaded definition, and audio link 
+        clearInput() {
+            this.userInput = '';
+            this.output = {};
+            this.audio = '';
+        },
+        // stores saved words to local storage, alerts user, and empties savedWords array
+        downloadWordList() {
+            const download = this.savedWords
+            localStorage.setItem('download', JSON.stringify(download));
+            console.log(download);
+            this.savedWords = [];
+            alert('You have successfully downloaded your saved words.')
+        },
+        // loads stored words from local storage
+        uploadWordList() {
+            const upload = JSON.parse(localStorage.getItem('download'));
+            this.savedWords = upload
+        },
     }})
