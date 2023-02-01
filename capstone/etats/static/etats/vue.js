3e8878e7b0ce138586c81d98d964636f8ab69c6c
Josh <24633211+JFC1@users.noreply.github.com>
2023-01-31 16:30:41 -0800

    working with views

--- capstone/etats/static/etats/vue.js
+++ capstone/etats/static/etats/vue.js
@@ -1,24 +1,70 @@
 new Vue({
     el: '#app',
     data: {
+        output: {},
         listItems: [],
         delete: '',
-        userItem: ''
+        userItem: '',
+        searchError: false,
+
+    },
+    delimiters: ['[[',']]'],
+    mounted: () => {
+        // this.getStateObject()
+        // // console.log('Test')
+            console.log('GET Request');
+            axios.get('/api')
+            // .then(response => console.log(response.data))
+            .then(response => this.listItems = response.data)
+                // this.listItems.push(response.data))
     },
     methods: {
+        // searches Free Dictionary API with user input and loads definition 
+        getStateObject() {
+            console.log('GET Request');
+            axios.get('/api')
+            .then(response => this.listItems = response.data)
+            // .meanings[0].definitions[0].definition)
+            .then(data => console.log(data))
+            // alerts user if user input is not found in dictionary
+            .catch(error => this.searchError = true && alert('Your search did not produce any results.  Please check your spelling and try again.'))
+        },
         // Appends user input to 'listItems' array and clears input field
-        addItem () {
+        addTaxItem () {
             this.listItems.push({
                 State: this.userItem,
                 completed: false,
               }),
             this.userItem = ''
         },
-        // Item text is lined-through if complete
-        itemComplete (todoItem) {
-            return {
-                completedItem: todoItem.completed
-            }},
+        addPoliticsItem () {
+            this.listItems.push({
+                State: this.userItem,
+                completed: false,
+              }),
+            this.userItem = ''
+        },
+        addHousingItem () {
+            this.listItems.push({
+                State: this.userItem,
+                completed: false,
+              }),
+            this.userItem = ''
+        },
+        addCrimeItem () {
+            this.listItems.push({
+                State: this.userItem,
+                completed: false,
+              }),
+            this.userItem = ''
+        },
+        addSearchItem () {
+            this.listItems.push({
+                State: this.userItem,
+                completed: false,
+              }),
+            this.userItem = ''
+        },
         // Item is marked complete/incomplete
         boxChecked (todoItem) {
             let todoIndex = this.listItems.indexOf(todoItem)
