501658d333808272b18302988c81611722eb101e
Josh <24633211+JFC1@users.noreply.github.com>
2023-02-03 11:38:57 -0800

    updated view and HTML files to add states to an array of states if an add button is clicked after selecting states via checkbox, regardless of array name

--- capstone/etats/static/etats/vue.js
+++ capstone/etats/static/etats/vue.js
@@ -1,114 +1,64 @@
 new Vue({
-    el: '#app',
-    data: {
-      userItem: '',
-      listItems: [],
-      taxList: [],
-      politicsList: [],
-      housingList: [],
-      crimeList: [],
-      active: false,
-    },
-    delimiters: ['[[',']]'],
-    mounted() {
-      this.getStateObject();
-    },
-    methods: {
-        // searches API and loads state information
-        getStateObject() {
-            console.log('GET Request');
-            axios.get('/api')
-            .then(response => this.listItems = response.data)
-            .then(data => console.log(data))
-            .catch(error => console.error(error));
-        },
-        onSubmit() {
-        // set active to true when form is submitted
-        this.active = true;
-        },
-        // Appends user input to 'listItems' array and clears input field
-        addTaxItem (state) {
-            let stateName = state.state_name;
-            let index = this.taxList.indexOf(stateName);
-            
-            if (index === -1) {
-              this.taxList.push(stateName);
-              console.log(this.taxList);
-              alert(`You have added ${stateName} to your favorites.`)
-            } else {
-              this.taxList.splice(index, 1);
-              console.log(this.taxList);
-              alert(`You have removed ${stateName} from your favorites.`)
-            }
-        },
-        addPoliticsItem (state) {
-            let stateName = state.state_name;
-            let index = this.politicsList.indexOf(stateName);
-            
-            if (index === -1) {
-              this.politicsList.push(stateName);
-              console.log(this.politicsList);
-              alert(`You have added ${stateName} to your favorites.`)
-            } else {
-              this.politicsList.splice(index, 1);
-              console.log(this.politicsList);
-              alert(`You have removed ${stateName} from your favorites.`)
-            }        },
-        addHousingItem (state) {
-            let stateName = state.state_name;
-            let index = this.housingList.indexOf(stateName);
-            
-            if (index === -1) {
-              this.housingList.push(stateName);
-              console.log(this.housingList);
-              alert(`You have added ${stateName} to your favorites.`)
-            } else {
-              this.housingList.splice(index, 1);
-              console.log(this.housingList);
-              alert(`You have removed ${stateName} from your favorites.`)
-            }
-        },
-        addCrimeItem (state) {
-            let stateName = state.state_name;
-            let index = this.crimeList.indexOf(stateName);
-            
-            if (index === -1) {
-              this.crimeList.push(stateName);
-              console.log(this.crimeList);
-              alert(`You have added ${stateName} to your favorites.`)
-            } else {
-              this.crimeList.splice(index, 1);
-              console.log(this.crimeList);
-              alert(`You have removed ${stateName} from your favorites.`)
-            }
-        },
-        // Item is marked complete/incomplete
-        boxChecked (todoItem) {
-            let todoIndex = this.listItems.indexOf(todoItem)
-            this.listItems[todoIndex].completed = !this.listItems[todoIndex].completed
-        },
-        // Item is deleted from list
-        deleteItem (todoItem) {
-            this.listItems.splice(todoItem, 1)
-        },
-        // clears unsaved user input from search field, loaded definition, and audio link 
-        clearInput() {
-            this.userInput = '';
-            this.output = {};
-            this.audio = '';
-        },
-        // stores saved words to local storage, alerts user, and empties savedWords array
-        downloadWordList() {
-            const download = this.savedWords
-            localStorage.setItem('download', JSON.stringify(download));
-            console.log(download);
-            this.savedWords = [];
-            alert('You have successfully downloaded your saved words.')
-        },
-        // loads stored words from local storage
-        uploadWordList() {
-            const upload = JSON.parse(localStorage.getItem('download'));
-            this.savedWords = upload
-        },
-    },
+  el: '#app',
+  data: {
+    userItem: '',
+    listItems: [],
+    taxList: [],
+    politicsList: [],
+    housingList: [],
+    crimeList: [],
+    selectedStates: [],
+    active: false,
+  },
+  delimiters: ['[[',']]'],
+  mounted() {
+    this.getStateObject();
+  },
+  methods: {
+      // searches API and loads state information
+      getStateObject() {
+          console.log('GET Request');
+          axios.get('/api')
+          .then(response => this.listItems = response.data)
+          .then(data => console.log(data))
+          .catch(error => console.error(error));
+      },
+      onSubmit() {
+      // set active to true when form is submitted
+      this.active = true;
+      },
+      addSelectedStates(listName) {
+        // clear the list
+        this[listName] = [];
+        // loop through the selected states and add them to list
+        for (const state of this.selectedStates) {
+            this[listName].push(state.state_name);
+        }
+        console.log(this[listName]);
+        alert(`You have updated your ${listName.split("List")[0]} favorites to the following: ${this[listName]}.`)
+
+      },
+      // Add/remove state from selectedStates array based on checkbox value
+      addItem(listName, state) {
+        const index = this.selectedStates.indexOf(state);
+        if (index === -1) {
+            this.selectedStates.push(state);
+        } else {
+            this.selectedStates.splice(index, 1);
+        }
+      },
+      // stores saved words to local storage, alerts user, and empties savedWords array
+      downloadWordList() {
+        const download = this.savedWords
+        localStorage.setItem('download', JSON.stringify(download));
+        console.log(download);
+        this.savedWords = [];
+        alert('You have successfully downloaded your saved words.')
+      },
+      // loads stored words from local storage
+      uploadWordList() {
+        const upload = JSON.parse(localStorage.getItem('download'));
+        this.savedWords = upload
+      },
+  },
 })
