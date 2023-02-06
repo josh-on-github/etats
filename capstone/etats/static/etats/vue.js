cc45ba96b68624059020db6cc0cf7d6091975689
Josh <24633211+JFC1@users.noreply.github.com>
2023-02-06 10:30:51 -0800

    added comments to vue methods

--- capstone/etats/static/etats/vue.js
+++ capstone/etats/static/etats/vue.js
@@ -1,21 +1,22 @@
 new Vue({
   el: '#app',
   data: {
-    userItem: '',
-    listItems: [],
-    taxList: [],
-    taxSortOrder: false,
+    userItem: '', // string to store the user's input for searching (index.html only)
+    listItems: [], // array to store the state objects fetched from the API
+    taxList: [], // array to store the user's selected tax states
+    taxSortOrder: false, // variable to determine the sort order of tax list
     politicsList: [],
     housingList: [],
     housingSortOrder: false,
     crimeList: [],
     crimeSortOrder: false,
-    selectedStates: [],
+    selectedStates: [], // array to store the selected states from checkboxes
     active: false,
   },
   delimiters: ['[[',']]'],
   mounted() {
     this.getStateObject();
+    // Try to retrieve the user's selected states from local storage
     try {
     this.taxList = JSON.parse(localStorage.getItem('taxList')) || [];
     this.politicsList = JSON.parse(localStorage.getItem('politicsList')) || [];
@@ -29,8 +30,8 @@ new Vue({
       // searches API and loads state information
       getStateObject() {
           console.log('GET Request');
-          axios.get('/api')
-          .then(response => this.listItems = response.data)
+          axios.get('/api') // send a GET request to the '/api' endpoint
+          .then(response => this.listItems = response.data) // store the data in the listItems array
           .then(data => console.log(data))
           .catch(error => console.error(error));
       },
@@ -46,7 +47,6 @@ new Vue({
             this[listName].push(state.state_name);
         }
         console.log(this[listName]);
-        // Overwrite the lists in localStorage
         alert(`You have updated your ${listName.split("List")[0]} favorites to the following: ${this[listName]}.`)
         // Store the list to localStorage
         localStorage.setItem(listName, JSON.stringify(this[listName]));
@@ -54,8 +54,10 @@ new Vue({
       // Add/remove state from selectedStates array based on checkbox value
       addItem(listName, state) {
         const index = this.selectedStates.indexOf(state);
-        if (index === -1) {
+        // Add the state to the selectedStates array if it is not in the array already
+        if (index === -1) { // -1 means that 'index' is not present in the array
             this.selectedStates.push(state);
+        // Remove the state from the selectedStates array if it is in the array
         } else {
             this.selectedStates.splice(index, 1);
         }
@@ -65,30 +67,24 @@ new Vue({
         this.taxSortOrder = !this.taxSortOrder;
         // Sort the `listItems` array based on the value of `taxSortOrder`
         // If `taxSortOrder` is `false`, sort in descending order by tax rate
-        this.listItems.sort((a, b) => {
+        this.listItems.sort((a, b) => { //comparison function returns the difference between the tax rate of two items (a and b).
         return this.taxSortOrder ? a.tax_rate - b.tax_rate : b.tax_rate - a.tax_rate;
         });
       },
       sortHousingList() {
-        // Toggle the value of `housingSortOrder`
         this.housingSortOrder = !this.housingSortOrder;
-        // Sort the `listItems` array based on the value of `housingSortOrder`
-        // If `housingSortOrder` is `false`, sort in descending order by median home value
         this.listItems.sort((a, b) => {
         return this.housingSortOrder ? a.median_home_value - b.median_home_value : b.median_home_value - a.median_home_value;
         });
       },
       sortCrimeList() {
-        // Toggle the value of `crimeSortOrder`
         this.crimeSortOrder = !this.crimeSortOrder;
-        // Sort the `listItems` array based on the value of `crimeSortOrder`
-        // If `crimeSortOrder` is `false`, sort in descending order by crime rate
         this.listItems.sort((a, b) => {
         return this.crimeSortOrder ? a.crime_rate - b.crime_rate : b.crime_rate - a.crime_rate;
         });
       },
       sortPoliticsList() {
-        // Compares the political affiliation of two states (a and b)
+        // Compares the political affiliation of two states (a and b) and returns the following order
         this.listItems.sort((a, b) => {
           // If the political affiliation of state 'a' is greater than (later in alphabetical order) that of state 'b', return 1.
           if (a.political_affiliation > b.political_affiliation) return 1;
@@ -100,26 +96,33 @@ new Vue({
       },
   },
   computed: {
+    // Returns a sorted version of the `listItems` array
     sortedList() {
+      // Sort the `listItems` array in descending order by the value of `count`
       return this.listItems.sort((a, b) => {
         return b.count - a.count;
       });
     },
     favoritesList() {
       let result = [];
+      // Loop through each item in `listItems`
       this.listItems.forEach(item => {
         let count = 0;
+        // If the item's `state_name` is included in the named arrays, increment `count`
         if (this.taxList.includes(item.state_name)) count++;
         if (this.politicsList.includes(item.state_name)) count++;
         if (this.housingList.includes(item.state_name)) count++;
         if (this.crimeList.includes(item.state_name)) count++;
+        // If `count` is greater than 0, add the item to `result`
         if (count > 0) {
           item.count = count;
           result.push(item);
         }
       });
+      // Sort the `result` array in descending order by the value of `count`
       return result.sort((a, b) => b.count - a.count);
     }
     }
   }
 )
+
